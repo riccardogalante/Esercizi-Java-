f2a04ce07dce9a43a4f77e119822cc1fbf181ed5
Rykard <rickygalante@hotmail.it>
2023-09-29 19:02:29 +0200

    ho aggiunto gli esercizi 2,3,4

---
+++ esercizio 2/src/Start.java
@@ -0,0 +1,10 @@
+public class Start {
+    public static void main(String[] args) {
+        char a= 'a';
+        char b= 'b';
+
+
+
+        System.out.println("a+b= "+ a + b);
+    }
+}
---
+++ esercizio 3/src/start.java
@@ -0,0 +1,10 @@
+public class start {
+    public static void main(String[] args) {
+        double x= 1.9;
+        double y= 3.2;
+        int z= (int) x + (int) y;
+        int w= (int) (x+y);
+        System.out.println("(int) x + (int) y= " +z);
+        System.out.println("(int) (x+y)=" +w);
+    }
+}
