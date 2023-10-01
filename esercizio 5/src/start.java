5f1923f6e8be6948bea2641b08a4d7a62ead70dc
Rykard <rickygalante@hotmail.it>
2023-10-01 21:18:19 +0200

    aggiornamento esercizi

---
+++ esercizio 5/src/start.java
@@ -0,0 +1,19 @@
+public class start {
+    public static void main(String[] args) {
+
+        int x = 6;
+        int y = 4;
+        sum(x,y);
+    }
+    public static int sum (int x, int y) {
+        int z=  x +  y;
+
+        System.out.println("x= " + x);
+        System.out.println("y= " + y);
+        System.out.println("sum of x+y= " + z);
+        return z;
+    }
+}
+
+
+
