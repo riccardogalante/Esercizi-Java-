17d78059642595eca135a821ec1d98444dd3bbd8
Rykard <rickygalante@hotmail.it>
2023-10-14 11:57:56 +0200

    esercizio 32

---
+++ esercizio 32/src/Forma.java
@@ -0,0 +1,16 @@
+public class Forma {
+    public int base;
+    public int altezza;
+
+    public Forma( int base,int altezza){
+        this.base=base;
+        this.altezza=altezza;
+    }
+    public int calcolaArea(){
+        return 0;
+
+    }
+
+
+}
+
