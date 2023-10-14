2ce15fa8e31b4490d6ac682078d22c93c8c7fa81
Rykard <rickygalante@hotmail.it>
2023-10-14 11:29:46 +0200

    esercizio 31

---
+++ esercizio 31/src/Gatto.java
@@ -0,0 +1,8 @@
+public class Gatto extends Animale {
+
+    @Override
+    public void faiIlVerso() {
+        super.faiIlVerso();
+        System.out.println("il gatto fa miao miao");
+    }
+}
---
+++ esercizio 31/src/Start.java
@@ -0,0 +1,13 @@
+public class Start {
+    public static void main(String[] args) {
+        Animale animale=new Animale();
+        System.out.println(animale);
+
+        Gatto gatto=new Gatto();
+        gatto.faiIlVerso();
+
+
+
+    }
+
+}
