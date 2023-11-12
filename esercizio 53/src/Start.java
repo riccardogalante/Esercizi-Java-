4487d138ac9ccb0150df242f0191f1c79e9203e0
Rykard <rickygalante@hotmail.it>
2023-11-12 23:06:14 +0100

    aggiornamento

---
+++ esercizio 53/src/Start.java
@@ -0,0 +1,13 @@
+public class Start {
+    public static void main(String[] args) {
+
+        Punto punto = new Punto(3, 5);
+
+
+        System.out.println("Coordinate x: " + punto.x());
+        System.out.println("Coordinate y: " + punto.y());
+
+
+        System.out.println("Oggetto Punto: " + punto);
+    }
+}
