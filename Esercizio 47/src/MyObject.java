8d18b45c8663bff59d881174a1fef78c84542585
Rykard <rickygalante@hotmail.it>
2023-11-12 18:45:12 +0100

    aggiornamento

---
+++ Esercizio 47/src/MyObject.java
@@ -0,0 +1,37 @@
+ class MyObject {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        MyObject myObject = (MyObject) obj;
+        return id == myObject.id;
+    }
+
+    @Override
+    public int hashCode() {
+
+        return id;
+    }
+}
+
