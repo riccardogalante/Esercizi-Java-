8b6a8c894ce3516af300191f9edc80193063a682
Rykard <rickygalante@hotmail.it>
2023-11-21 12:37:36 +0100

    esercizio 5

---
+++ esercizio 5 java 20/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int x=20;
+        int y=10;
+        System.out.println(calcoloSomma(x,y));
+
+
+    }
+    public static int calcoloSomma (int num1, int num2){
+        int somma= num1+num2;
+        return somma;
+    }
+}
