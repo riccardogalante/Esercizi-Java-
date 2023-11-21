41d97d3c26dc9b206ac5bbb5b585a770507b3e2b
Rykard <rickygalante@hotmail.it>
2023-11-21 12:11:10 +0100

    esercizio 3

---
+++ esercizio 3 java 20/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double x=9.3;
+        double y=4.9;
+        int z= (int) x+(int)y;
+        System.out.println("la somma convertita da double a int di x e y è: "+z);
+    }
+}
