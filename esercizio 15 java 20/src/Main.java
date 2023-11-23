5ac35ca09cb0a980127e1cdc0b9bab9d20c42b6a
Rykard <rickygalante@hotmail.it>
2023-11-23 20:09:14 +0100

    esercizio 15

---
+++ esercizio 15 java 20/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        int num= 15;
+        stampaFizzBuzz(num);
+    }
+    public static void stampaFizzBuzz(int numero){
+
+        if (numero%3==0 && numero%5==0){
+            System.out.println("FizzBuzz");
+        } else if (numero%3==0) {
+            System.out.println("Fizz");
+        } else if (numero%5==0) {
+            System.out.println("Buzz");
+        }
+    }
+}
+
+
+
+
+
+//    Scrivere un programma che dato un valore inserito dall'utente o dichiarato in una variabile, utilizzi un metodo che per i multipli di 3 stampi "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz".
+//        Nel caso in cui un numero sia multiplo di entrambi stampi "FizzBuzz".
+
