cad98ce583a28495b4f153b6d8c7a5fd5ddab13d
Rykard <rickygalante@hotmail.it>
2023-12-10 22:11:46 +0100

    esercizio 36

---
+++ esercizio 36 java 20/src/Triangolo.java
@@ -0,0 +1,17 @@
+class Triangolo implements Forma {
+    private double base;
+    private double altezza;
+
+    // Costruttore
+    public Triangolo(double base, double altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    // Implementazione del metodo calcolaArea per il triangolo
+    @Override
+    public void calcolaArea() {
+        double area = 0.5 * base * altezza;
+        System.out.println("L'area del triangolo è: " + area);
+    }
+}
